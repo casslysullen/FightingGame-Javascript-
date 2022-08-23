d1f74e8e2fe4425cfadd01052f8451e5859dd00f
casslysullen <58861125+casslysullen@users.noreply.github.com>
2022-08-23 15:03:26 -0400

    Add files via upload

---
+++ Javascript Samurai/index.js
@@ -0,0 +1,259 @@
+const canvas=document.querySelector('canvas');
+//C stands for context
+const c= canvas.getContext('2d');
+
+canvas.width= 1024;
+canvas.height= 576;
+
+c.fillRect(0,0, canvas.width, canvas.height);
+
+const gravity= 0.7;
+//Creating Player and Enemy
+class Sprite {
+    constructor({position, velocity, color= 'red', offset}) {
+        this.position = position,
+        this.velocity = velocity,
+        this.width= 50
+        this.height = 150,
+        this.lastKey,
+        this.attackBox = 
+        {
+            position: {
+                x: this.position.x,
+                y: this.position.y
+            },
+            offset,
+            width: 100,
+            height: 50
+        },
+        this.color= color,
+        this.isAttacking,
+        this.health= 100;
+    }
+
+    draw() {
+        c.fillStyle= this.color;
+        c.fillRect(this.position.x, this.position.y, 50, this.width, this.height);
+        
+        //Attack Box
+        if(this.isAttacking){
+        c.fillStyle='green';
+        c.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height);}
+        
+    }
+
+    update() {
+        this.draw()
+        this.attackBox.position.x= this.position.x + this.attackBox.offset.x;
+        this.attackBox.position.y= this.position.y;
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+
+        if (this.position.y +this.height + this.velocity.y >= canvas.height) {
+            this.velocity.y= 0
+        }
+        else  this.velocity.y += gravity
+    }
+    
+    attack() {
+        this.isAttacking=true;
+        setTimeout(() => {
+            this.isAttacking=false
+        }, 100)
+    }
+}
+
+
+
+const player = new Sprite({
+    position: {
+        x:0,
+    y:0
+},
+    velocity: {
+        x:0, 
+        y:0
+    },
+    offset: {
+        x:0,
+        y:0
+    }
+});
+
+const enemy = new Sprite({
+    position: {
+        x:400,
+    y:100
+},
+    velocity: {
+        x:0, 
+        y:0
+    },
+
+    offset: {
+        x:-50,
+        y:0
+    },
+
+    color: 'blue'
+});
+
+
+//This is to execute the draw function which will show the height and width of the player.
+player.draw();
+enemy.draw();
+//This is to display that player is officially created
+console.log(player)
+
+const keys= {
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+    w: {
+        pressed: false
+    },
+    ArrowLeft:{
+        pressed: false
+    },
+
+    ArrowRight: {
+        pressed: false
+    }
+    
+}
+
+function rectangularCollision({rectangle1, rectangle2}) {
+    return (
+        rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x && 
+        rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width && 
+        rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y && 
+        rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
+    )
+}
+
+let timer= 100
+function decreaseTimer() {
+if (timer > 0) {
+    setTimeout(decreaseTimer, 1000)
+    timer -- 
+document.querySelector('#timer').innerHTML= timer
+}
+if (player.health===enemy.health) {
+    document.querySelector('#displayText').innerHTML='Tie';
+    document.querySelector('#displayText').style.display='flex';
+// } else if (player.health > enemy.health) {
+//     console.log("player wins!")
+// } else (player.health < enemy.health) {
+//     console.log("enemy wins!")
+}
+
+}
+
+
+function animate() {
+    window.requestAnimationFrame(animate)
+    c.fillStyle= "black"
+    c.fillRect(0, 0, canvas.width, canvas.height)
+    player.update();
+    enemy.update();
+
+    player.velocity.x=0
+    enemy.velocity.x=0
+
+    //player movement
+    if (keys.a.pressed && player.lastKey === 'a') {
+        player.velocity.x= -5
+    } else if (keys.d.pressed && player.lastKey === 'd') {
+        player.velocity.x=5
+    }
+
+    //enemy movement
+    if (keys.ArrowLeft.pressed && enemy.lastKey === 'ArrowLeft') {
+        enemy.velocity.x= -5
+    } else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight') {
+        enemy.velocity.x=5
+    }
+    //detect for collision
+    if(rectangularCollision({
+        rectangle1:player,
+        rectangle2: enemy
+    }) &&
+        player.isAttacking) 
+        {player.isAttacking=false
+            enemy.health -= 20
+            document.querySelector('#enemyHealth').style.width= enemy.health + '%'
+    }
+
+    if(rectangularCollision({
+        rectangle1:enemy,
+        rectangle2: player
+    }) &&
+        enemy.isAttacking)
+        {enemy.isAttacking=false
+            player.health -= 20
+            document.querySelector('#playerHealth').style.width= player.health + '%'
+    }
+}
+
+animate()
+
+window.addEventListener('keydown', (event)=> {
+    
+    switch(event.key) {
+        case 'd':
+            keys.d.pressed= true
+            player.lastKey='d'
+            break
+        case 'a':keys.a.pressed= true
+            player.lastKey='a'
+            break
+        //Helps the player to jump
+        case 'w':
+            player.velocity.y=-20
+            break
+        case 's':
+            player.attack() 
+            break
+        case 'ArrowRight':
+            keys.ArrowRight.pressed= true
+            enemy.lastKey='ArrowRight'
+            break
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed= true
+            enemy.lastKey='ArrowLeft'
+            break
+        //Helps the player to jump
+        case 'ArrowUp':
+            enemy.velocity.y=-20
+            break
+        case 'ArrowDown':
+            enemy.isAttacking= true
+            break
+    }
+    
+})
+
+window.addEventListener('keyup', (event)=> {
+    switch(event.key) {
+        case 'd':
+            keys.d.pressed= false
+        break
+        case 'a':
+            keys.a.pressed= false
+        break
+        case 'ArrowRight':
+            keys.ArrowRight.pressed= false
+        break
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed= false
+        break
+        //Helps the player to jump
+        case 'ArrowUp':
+            player.velocity.y=-10
+        break
+    }
+    
+})
